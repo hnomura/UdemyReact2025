ef30b954450ce789f604f3c1645f62cf438fdffa
hnomura <hnomura.babou@gmail.com>
2025-02-28 16:21:25 +0900

    Using map() to array (module 58)

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react"; 
+import { useState } from "react";
 
 import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import Header from "./components/Header/Header";
@@ -8,24 +8,23 @@ import TabButton from "./components/TabButton";
 function App() {
   const [selectedTopic, setSelectedTopic] = useState();
 
-  function handleSelect(selectedButton) { 
+  function handleSelect(selectedButton) {
     // selectedButton => 'components', 'jsx', props', 'state'
     // console.log(selectedButton);
-    setSelectedTopic(selectedButton); 
+    setSelectedTopic(selectedButton);
   }
 
   let tabContent = <p>Please select a topic</p>;
-  if (selectedTopic) { 
+  if (selectedTopic) {
     tabContent = (
-    <div id="tab-content">
-      <h3>{EXAMPLES[selectedTopic].title}</h3>
-      <p>{EXAMPLES[selectedTopic].description}</p>
-      <pre>
-        <code>
-          {EXAMPLES[selectedTopic].code}
-        </code>
-      </pre>
-    </div> )
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -35,26 +34,43 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            {/* basic property passing */}
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            {/* property via spread operator */ }
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton isSelected={selectedTopic=="components"} onSelect={() => handleSelect('components')}> Components </TabButton>
-            <TabButton isSelected={selectedTopic=="jsx"} onSelect={() => handleSelect('jsx')}> JSX </TabButton>
-            <TabButton isSelected={selectedTopic=="props"} onSelect={() => handleSelect('props')}> Props </TabButton>
-            <TabButton isSelected={selectedTopic=="state"} onSelect={() => handleSelect('state')}> State </TabButton>
-          </menu> 
+            <TabButton
+              isSelected={selectedTopic == "components"}
+              onSelect={() => handleSelect("components")}
+            >
+              {" "}
+              Components{" "}
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic == "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
+              {" "}
+              JSX{" "}
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic == "props"}
+              onSelect={() => handleSelect("props")}
+            >
+              {" "}
+              Props{" "}
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic == "state"}
+              onSelect={() => handleSelect("state")}
+            >
+              {" "}
+              State{" "}
+            </TabButton>
+          </menu>
           {tabContent}
         </section>
         <h2>Time to get started for react!</h2>
