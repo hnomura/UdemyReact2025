dff74fba2a03a1adef38f69a52cfea4830ba2800
Hiroshi Nomura <hnomura.babou@gmail.com>
2025-03-03 18:31:50 +0900

    Section 4: 69. Default Prop value

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -28,7 +28,7 @@ export default function Examples() {
   return (
     <Section title="Examples" id="examples">
       <Tabs
-        buttonsContainer="menu"
+        // ButtonsContainer="menu" (--default is "menu"--)
         buttons={
           <>
             <TabButton
--- 01-starting-project/src/components/Tabs.jsx
+++ 01-starting-project/src/components/Tabs.jsx
@@ -1,13 +1,13 @@
-export default function Tabs({ children, buttons, buttonsContainer }) {
+export default function Tabs({ children, buttons, ButtonsContainer = "menu" }) {
   // dynamically specified component type for wrapper of "buttons"
   // Key is to use the Upper Case character for the first letter, so that
   // React treats it as a component instead of a string.
   // Alternatively, the prop name can be "ButtonContainer" in this case,
   // without the const declaration below.
-  const ButtonContainer = buttonsContainer;
+  // const ButtonContainer = buttonsContainer;
   return (
     <>
-      <ButtonContainer>{buttons}</ButtonContainer>
+      <ButtonsContainer>{buttons}</ButtonsContainer>
       {children}
     </>
   );
