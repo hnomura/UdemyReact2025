dd497527200879fe13f7f372d2887bfbe0c8c4fd
Hiroshi Nomura <hnomura.babou@gmail.com>
2025-03-03 17:26:46 +0900

    Section 4: 68. Set Component Types Dynamically

--- 01-starting-project/src/components/Tabs.jsx
+++ 01-starting-project/src/components/Tabs.jsx
@@ -1,7 +1,13 @@
-export default function Tabs({ children, buttons }) {
+export default function Tabs({ children, buttons, buttonsContainer }) {
+  // dynamically specified component type for wrapper of "buttons"
+  // Key is to use the Upper Case character for the first letter, so that
+  // React treats it as a component instead of a string.
+  // Alternatively, the prop name can be "ButtonContainer" in this case,
+  // without the const declaration below.
+  const ButtonContainer = buttonsContainer;
   return (
     <>
-      <menu>{buttons}</menu>
+      <ButtonContainer>{buttons}</ButtonContainer>
       {children}
     </>
   );
